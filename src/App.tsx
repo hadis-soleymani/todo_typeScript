4428feb2f1ad3403ed225731089605bd51f621cc
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-26 07:18:55 -0700

    add type of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-let name:string = "Hadis";
-
-function App() {
-  return (
-    <div >
-      {name}
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div></div>;
+};
 
 export default App;

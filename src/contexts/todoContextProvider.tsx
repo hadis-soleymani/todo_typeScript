8cf3f2a201870d48f9f5f0a64c43e346c4a947aa
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-29 06:40:31 -0700

    add comment

--- src/contexts/todoContextProvider.tsx
+++ src/contexts/todoContextProvider.tsx
@@ -1,69 +1,73 @@
 import React, { createContext, useReducer } from "react";
 
+//interface of one todo object
 interface Todo {
   id: number;
   todo: string;
   isDone: boolean;
 }
-const initialState = [
-  {
-    id: 0,
-    todo: "t",
-    isDone: false,
-  },
-];
 
+//we define type of actions this way
 type ACTIONTYPE =
   | { type: "remove"; payload: number }
   | { type: "add"; payload: string }
-  | { type: "edit"; payload: {id:number, todo:string} }
+  | { type: "edit"; payload: { id: number; todo: string } }
   | { type: "done"; payload: number };
 
+//create reducer
 const TodoReducer = (state: Todo[], action: ACTIONTYPE) => {
-  console.log(state);
   switch (action.type) {
+    //add one todo object
     case "add":
       return [
         ...state,
         { id: Date.now(), todo: action.payload, isDone: false },
       ];
 
+    //remove one todo object
     case "remove":
       return state.filter((todoObj) => todoObj.id != action.payload);
 
+    //done one todo object
     case "done":
-      return state.map((todo) =>
-        todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo
+      return state.map((todoObj) =>
+        todoObj.id === action.payload
+          ? { ...todoObj, isDone: !todoObj.isDone }
+          : todoObj
       );
 
+    //edit todo part of todo object
     case "edit":
-      return state.map((todo) =>
-        todo.id === action.payload.id
-          ? { ...todo, todo: action.payload.todo }
-          : todo
+      return state.map((todoObj) =>
+        todoObj.id === action.payload.id
+          ? { ...todoObj, todo: action.payload.todo }
+          : todoObj
       );
     default:
       return state;
   }
 };
 
+//input type of create context
+interface InputCreateContext {
+  state: Todo[];
+  dispatch: React.Dispatch<ACTIONTYPE>;
+}
+
+//create a context with empty initialState (state: [])
+export const todoContext = createContext<InputCreateContext>({
+  state: [],
+  dispatch: () => {},
+});
+
+//define type of children
 type ProviderProps = {
   children: React.ReactNode;
 };
 
-export const todoContext = createContext<{
-  state: Todo[];
-  dispatch: React.Dispatch<ACTIONTYPE>;
-}>({ state: [], dispatch: () => {} });
-
-// export const todoContext = createContext<Todo[] | null>(null);
-
-type d = {
-  todosState: Todo[];
-  dispatch: React.Dispatch<ACTIONTYPE>;
-};
-
+//create context provider for wrapping App component
 const TodoContextProvider = (props: ProviderProps) => {
+  //[] is initialState
   const [state, dispatch] = useReducer(TodoReducer, []);
 
   return (
